cc81b29998c6122d5ed9e6731fcb4985e90b9cc7
Yazan. <73472040+NotYazan@users.noreply.github.com>
2025-06-28 18:08:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const { Client, Events,  } = require('discord.js');
-const client = new Client({
-    intents: 131071,
-});
-
-client.on(Events.ClientReady, async () => {
-    console.log(client.user.tag)
-}).login("MTA1MDA3NTc1NjAwNjQ4MTkzMQ.GZre6s.ArnJu634AFp0n15VDhIAyH4MLW8fxyvmUQztXE");
+No
